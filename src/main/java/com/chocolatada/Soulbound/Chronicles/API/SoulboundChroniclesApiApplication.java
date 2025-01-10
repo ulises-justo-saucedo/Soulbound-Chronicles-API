1cac7030566e5a6346be3903376732e63f254174
Ulises Justo Saucedo <ulisesjustosaucedo@gmail.com>
2025-01-10 14:30:18 -0300

    All starts here!

---
+++ src/main/java/com/chocolatada/Soulbound/Chronicles/API/SoulboundChroniclesApiApplication.java
@@ -0,0 +1,13 @@
+package com.chocolatada.Soulbound.Chronicles.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulboundChroniclesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulboundChroniclesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chocolatada/Soulbound/Chronicles/API/SoulboundChroniclesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chocolatada.Soulbound.Chronicles.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulboundChroniclesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

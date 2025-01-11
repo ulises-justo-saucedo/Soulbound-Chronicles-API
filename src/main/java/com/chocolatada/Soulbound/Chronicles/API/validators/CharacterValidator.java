0090daf1204c832bb920afdc63c707cf28be0d9b
Ulises Justo Saucedo <ulisesjustosaucedo@gmail.com>
2025-01-11 17:37:19 -0300

    Constants for character and user validator

--- src/main/java/com/chocolatada/Soulbound/Chronicles/API/validators/CharacterValidator.java
+++ src/main/java/com/chocolatada/Soulbound/Chronicles/API/validators/CharacterValidator.java
@@ -5,9 +5,13 @@ import com.chocolatada.Soulbound.Chronicles.API.exceptions.character.CharacterIn
 import com.chocolatada.Soulbound.Chronicles.API.exceptions.character.CharacterInvalidName;
 
 public class CharacterValidator {
+    private final static int MIN_ID = 1;
+    private final static int MAX_NAME_LENGTH = 12;
+    private final static int MIN_NAME_LENGTH = 4;
+
     public static void validateId(long id, CharacterEntity character) throws CharacterInvalidId {
-        if(id < 1)
-            throw new CharacterInvalidId("ERROR: Id '"+id+"' cannot be less than 1.");
+        if(id < MIN_ID)
+            throw new CharacterInvalidId("ERROR: Id '"+id+"' cannot be less than '"+MIN_ID+"'.");
 
         if(character == null)
             throw new CharacterInvalidId("ERROR: Id '"+id+"' doesn't correspond to any character.");
@@ -15,6 +19,12 @@ public class CharacterValidator {
     public static void validateName(String name) throws CharacterInvalidName {
         if(name.contains(" "))
             throw new CharacterInvalidName("ERROR: Name cannot contain blank spaces.");
+
+        if(name.length() < MIN_NAME_LENGTH)
+            throw new CharacterInvalidName("ERROR: Name '"+name+"' cannot be less than "+MIN_NAME_LENGTH+" characters.");
+
+        if(name.length() > MAX_NAME_LENGTH)
+            throw new CharacterInvalidName("ERROR: Name '"+name+"' cannot be longer than "+MAX_NAME_LENGTH+" characters.");
 
         if(name.isEmpty())
             throw new CharacterInvalidName("ERROR: Name cannot be empty.");
--- src/main/java/com/chocolatada/Soulbound/Chronicles/API/validators/UserValidator.java
+++ src/main/java/com/chocolatada/Soulbound/Chronicles/API/validators/UserValidator.java
@@ -6,9 +6,14 @@ import com.chocolatada.Soulbound.Chronicles.API.exceptions.user.UserInvalidPassw
 import com.chocolatada.Soulbound.Chronicles.API.exceptions.user.UserInvalidUsername;
 
 public class UserValidator {
+    private final static int MIN_ID = 1;
+    private final static int MAX_USERNAME_LENGTH = 12;
+    private final static int MIN_USERNAME_LENGTH = 4;
+    private final static int MIN_PASSWORD_LENGTH = 6;
+
     public static void validateId(long id, UserEntity user) throws UserInvalidId {
-        if(id < 1)
-            throw new UserInvalidId("ERROR: Id '"+id+"' cannot be less than 1.");
+        if(id < MIN_ID)
+            throw new UserInvalidId("ERROR: Id '"+id+"' cannot be less than '"+MIN_ID+"'.");
 
         if(user == null)
             throw new UserInvalidId("ERROR: Id '"+id+"' doesn't correspond to any user.");
@@ -17,14 +22,17 @@ public class UserValidator {
         if(username.isEmpty())
             throw new UserInvalidUsername("ERROR: Username cannot be empty.");
 
-        if(username.length() > 11)
-            throw new UserInvalidUsername("ERROR: Username '"+username+"' cannot be longer than 11 characters.");
+        if(username.length() < MIN_USERNAME_LENGTH)
+            throw new UserInvalidUsername("ERROR: Username '"+username+"' cannot be shorter than '"+MIN_USERNAME_LENGTH+"' characters.");
+
+        if(username.length() > MAX_USERNAME_LENGTH)
+            throw new UserInvalidUsername("ERROR: Username '"+username+"' cannot be longer than '"+MAX_USERNAME_LENGTH+"' characters.");
     }
     public static void validatePassword(String password) throws UserInvalidPassword {
         if(password.isEmpty())
             throw new UserInvalidPassword("ERROR: Password cannot be empty.");
 
-        if(password.length() < 6)
-            throw new UserInvalidPassword("ERROR: Password '"+password+"' cannot be shorter than 6 characters.");
+        if(password.length() < MIN_PASSWORD_LENGTH)
+            throw new UserInvalidPassword("ERROR: Password cannot be shorter than '"+MIN_PASSWORD_LENGTH+"' characters.");
     }
 }
